4f8557a794cbd1555b7ed0cfb6c0406b604161b4
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-07 13:16:51 -0500

    main

--- mainFB.java
+++ mainFB.java
@@ -1,8 +1,31 @@
+import java.util.Scanner; //user input
+
 public class mainFB extends Flappy {
+	public static String namingFlappie(){
+		Scanner scan = new Scanner(System.in);
+		String name;
+		System.out.println("Ingrese el nombre de su flappy: ");
+		name = scan.next();
+		return name;
+	}
 	public static void main(String[] args) {
+		int color;
+		String name;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese un numero (color):");
+		color = scan.nextInt();
+		System.out.println("Si desea, usted puede nombrar a su Flappie.\nDesea nombrarlo? (s/n) ");
+		name = scan.next();
+		if (name != "s"){
+			name = namingFlappie();
+		} else {
+			name = "lol";
+		}
 		Flappy myFlappy = new Flappy();
-		myFlappy.setColour(4);
+		myFlappy.setColour(color);
+		myFlappy.setName(name);
 		myFlappy.volar();
+		myFlappy.hablar();
 	}
 
 }
